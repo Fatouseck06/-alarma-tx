a3b347ceadb1842d4289bc6a7a4cfc3b2d95d309
Fatouseck06 <125355360+Fatouseck06@users.noreply.github.com>
2023-06-01 09:45:37 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
-let puerta_abierta = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    puerta_abierta = receivedNumber
-    if (true) {
-        basic.showIcon(IconNames.Heart)
-    } else if (false) {
-        basic.showIcon(IconNames.Angry)
-    }
+    alarma = receivedNumber
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+let alarma = 0
+radio.setGroup(35)
 basic.forever(function () {
-	
+    if (alarma == 1) {
+        basic.showIcon(IconNames.Sad)
+    } else if (alarma == 0) {
+        basic.showIcon(IconNames.Happy)
+    }
 })
